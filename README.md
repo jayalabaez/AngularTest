fb1008a092b0aee0ceebcfee4aa8e533fded4161
jayalabaez <jayalabaez@gmail.com>
2019-11-12 16:36:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-muqnxe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-muqnxe)
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+
+@Component({
+selector : 'test-cm',
+templateUrl : './test.template.html'
+
+})
+
+export class TestComponent {
+testVar : string = 'Test Vars'
+
+
+}
