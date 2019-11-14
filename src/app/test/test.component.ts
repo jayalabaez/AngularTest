cdb435733b067e44e80cb93f47038e13d6b6e828
jayalabaez <jayalabaez@gmail.com>
2019-11-14 16:05:23 -0400

    2 files modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,6 +8,48 @@ templateUrl : './test.template.html'
 
 export class TestComponent {
 testVar : string = 'Test Vars'
+products : Array<Product> = Product.createProducts();
+show : boolean  = true;
+inputValue:string;
+
+insertData():void{
+if(this.inputValue){this.products.push(new Product({name:this.inputValue,id:0}));
+this.inputValue = "";}
+
+};
+
+toogle():void{ 
+
+this.show = !this.show;
+};
+
+delete(index){
+this.products.splice(index,1);
+
+};
+
+}
+
+ class Product{
+name:string = "fff";
+id:number = 90;
+
+   public constructor(init?:Partial<Product>) {
+        Object.assign(this, init);
+    }
+
+public static createProducts(): Array<Product>
+{
+let buffer : Array<Product> =new Array<Product>(); 
+
+
+
+for(let i = 0; i<10; i++){
+let toAdd = new Product({name:(String(i)+"joel"), id:i});
+buffer.push(toAdd);
+}
+return buffer;
+};
 
 
 }
